55f1c11fbfe6bf0d88a5e02bd3bd3b4b1fe9bf53
gimnadaumesh <umesh@axis-tech.co>
2023-11-09 14:20:34 +0530

    chaned id

--- infrastructure/route_table.tf
+++ infrastructure/route_table.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table_association" "main" {
     subnet_id = aws_subnet.main.id
-    route_table_id =aws_vpc.main.id
+    route_table_id =aws_vpc.main.default_route_table_id
 }
 
 
 
 resource "aws_route" "main" {
-    route_table_id = aws_vpc.main.id
+    route_table_id = aws_vpc.main.default_route_table_id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
 }
