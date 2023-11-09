e5230e1766c11b21cf83eb7b35a91960af8d0ccb
gimnadaumesh <umesh@axis-tech.co>
2023-11-09 12:31:13 +0530

    Creater infra

---
+++ infrastructure/ec2.tf
@@ -0,0 +1,17 @@
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "exetel assignment"
+
+  instance_type          = "t2.micro"
+  key_name               = "exetel"
+  monitoring             = true
+  vpc_security_group_ids = aws_security_group.allow_80.id
+  subnet_id              = aws_subnet.main.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
