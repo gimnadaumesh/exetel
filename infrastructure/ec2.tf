ef9712440c857d020c84dfb028e34cb5351c5b69
gimnadaumesh <umesh@axis-tech.co>
2023-11-09 13:04:47 +0530

    Added EIP

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_80.id]
   subnet_id              = aws_subnet.main.id
-  public_ip              = true
+  
 
   tags = {
     Terraform   = "true"
