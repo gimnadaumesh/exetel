ec31760c5341c1940172f4c22188ad2210bbe623
gimnadaumesh <umesh@axis-tech.co>
2023-11-09 13:26:21 +0530

    Cnages added for Routes

--- infrastructure/route_table.tf
+++ infrastructure/route_table.tf
@@ -8,6 +8,6 @@ resource "aws_route_table_association" "main" {
 resource "aws_route" "main" {
     route_table_id = aws_vpc.main.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.qw.id
+    gateway_id = aws_internet_gateway.gw.id
 }
 
