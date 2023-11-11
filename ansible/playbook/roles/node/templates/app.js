053f77a9316c614e1ba7c9d13cc130b8a45fc57a
gimnadaumesh <umesh@axis-tech.co>
2023-11-11 11:01:44 +0530

    Changerd App.js

--- ansible/playbook/roles/node/templates/app.js
+++ ansible/playbook/roles/node/templates/app.js
@@ -3,21 +3,25 @@ const AWS = require('aws-sdk');
 const app = express();
 
 AWS.config = new AWS.Config();
-AWS.config.accessKeyId = '${{ ACCESS_KEY }}';
-AWS.config.secretAccessKey = '${{ SECRET_KEY }}';
+AWS.config.accessKeyId = '{{ ACCESS_KEY }}';
+AWS.config.secretAccessKey = '{{ SECRET_KEY }}';
 AWS.config.region = "ap-southeast-2";
 
 const metadata = new AWS.MetadataService();
 
 app.get('/', async (req, res) => {
-  try {
-    const data = await metadata.request('/latest/meta-data/instance-id');
-    const instanceId = data.split('\n')[0];
-    res.send(`AWS EC2 Instance ID: ${instanceId}`);
-  } catch (error) {
-    console.error('Error retrieving instance ID:', error);
-    res.status(500).send('Internal Server Error');
-  }
+  var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+  var params = {
+    DryRun: false
+    };
+  ec2.describeInstances(params, function(err, data) {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log("Success", JSON.stringify(data));
+            res.send(`########### .......... AWS EC2 Instance ID: ${JSON.stringify(data.Reservations[0].Instances[0].InstanceId)}.......... ########### ..........   AWS EC2 Public IP: ${JSON.stringify(data.Reservations[0].Instances[0].PrivateIpAddress)}..........###########`);
+        }
+});
 });
 
 const port = 3000;
